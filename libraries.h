cfc4630f6f2492fdfb4bf4a383e2908ab5515521
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2022-12-13 16:03:39 +0100

    Create libraries.h

---
+++ libraries.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
