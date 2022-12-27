3240faca224a0c605dcc335a20dc5916f3283fed
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2022-12-27 15:47:43 +0100

    Added union semun

--- libraries.h
+++ libraries.h
@@ -34,7 +34,7 @@ union semun {
     struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
     unsigned short  *array;  /* Array for GETALL, SETALL */
     struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                               (Linux-specific) */
+                                (Linux-specific) */
 };
 
 typedef struct {
